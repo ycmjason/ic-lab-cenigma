3501be69f5519cabb88779b0debe816e97ffb00e
Jason Yu <jason.yu14@imperial.ac.uk>
2015-10-26 01:36:17 +0000

    Done

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,35 @@
-// skeleton C++ file, you will need to edit this and other files to implement your enigma machine
 #include <stdexcept>
 #include <iostream>
-#include <fstream>
 
-using namespace std;
+#include "classes/Enigma.hpp"
+#include "Utils.hpp"
 
-int main(int argc, char **argv)
-{
-  cout << "TODO: implement an Enigma machine" << endl;
+int main(int argc, char **argv){
+  const int ROTOR_COUNT = argc-2;
+  Enigma enigma(ROTOR_COUNT);
+  try{
+    enigma.setPlugboardByFilename(argv[argc-1]);
+  }catch(char *msg){
+    std::cout << msg << std::endl;
+  }
+  for(int i = 1; i <= ROTOR_COUNT; ++i){
+    enigma.addRotorByFilename(argv[i]);
+  }
+
+  /**** main loop for enigma ****/
+  char input;
+  while(std::cin >> input){
+    // skip the space characters
+    if(input ==  ' ' || input == '\t' ||
+       input == '\r' || input == '\n'){
+      std::cout << input;
+      continue;
+    }
+
+    std::cout << enigma.encrypt(input);
+  }
+
+  /**** clean up memory prevent memory leak ***/
+
   return 0;
 }
--- makefile
+++ makefile
@@ -1,12 +1,33 @@
 # edit this makefile so that running make compiles your enigma program
 
-enigma: Main.o
-	g++ -o enigma Main.o
+enigma: Main.o Enigma.o Plugboard.o Rotor.o RotorConnector.o Reflector.o Utils.o
+	g++ -o enigma Main.o Enigma.o Plugboard.o Rotor.o RotorConnector.o Reflector.o Utils.o
 
 Main.o: Main.cpp
 	g++ -c Main.cpp
 
+Enigma.o: classes/Enigma.cpp classes/Enigma.hpp
+	g++ -c classes/Enigma.cpp
+
+Plugboard.o: classes/Plugboard.cpp classes/Plugboard.hpp
+	g++ -c classes/Plugboard.cpp
+
+Rotor.o: classes/Rotor.cpp classes/Rotor.hpp
+	g++ -c classes/Rotor.cpp
+
+RotorConnector.o: classes/RotorConnector.cpp classes/RotorConnector.hpp
+	g++ -c classes/RotorConnector.cpp
+
+
+Reflector.o: classes/Reflector.cpp classes/Reflector.hpp
+	g++ -c classes/Reflector.cpp
+
+Utils.o: Utils.cpp Utils.hpp
+	g++ -c Utils.cpp
+
+all: enigma
+
 clean:
 	rm -rf enigma *.o
 
-.PHONY: clean
+.PHONY: clean all
